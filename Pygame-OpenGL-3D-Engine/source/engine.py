58831a8cc04e1c8176b55559812700f954a2f1c6
sheng <shengwangandy@foxmail.com>
2024-06-16 00:51:18 +0800

    补充运动控制逻辑

--- Pygame-OpenGL-3D-Engine/source/engine.py
+++ Pygame-OpenGL-3D-Engine/source/engine.py
@@ -20,6 +20,10 @@ class Engine(mglw.WindowConfig):
     resource_dir = (Path(__file__).parent / '../assets').resolve()
     shader_dir = (Path(__file__).parent / '../source/shaders').resolve()
 
+
+    SPEED = 1000
+    SENSITIVITY = 0.1
+
     aspect_ratio = None
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -43,6 +47,10 @@ class Engine(mglw.WindowConfig):
         globalAxes = GlobalAxes(self)
         self.scene.add_model(globalAxes)
 
+        self.mouseLeftPress = False
+        self.mouseRightPress = False
+        self.qweasd = {'Q': False, 'W': False, 'E':False, 'A':False, 'S':False, 'D': False }
+
     def setCamera(self, cam):
         self.camera = cam
     def setScene(self, scene):
@@ -55,6 +63,7 @@ class Engine(mglw.WindowConfig):
 
         if self.camera:
             self.camera.tick()
+            self.keyControlCamera(frametime)
 
         if self.scene:
             self.scene.tick()
@@ -86,27 +95,101 @@ class Engine(mglw.WindowConfig):
         imgui.render()
         self.imgui.render(imgui.get_draw_data())
 
+
     def resize(self, width: int, height: int):
         self.camera.resize(width, height)
         self.imgui.resize(width, height)
 
+    def keyControlCamera(self, frametime):
+        velocity = self.SPEED * frametime
+        if self.mouseRightPress == False:
+            return
+        if self.qweasd['W']:
+            self.camera.moveForwardBackward(-velocity)
+        elif self.qweasd['S']:
+            self.camera.moveForwardBackward(velocity)
+        if self.qweasd['A']:
+            self.camera.moveLeftRight(velocity)
+        elif self.qweasd['D']:
+            self.camera.moveLeftRight(-velocity)
+        if self.qweasd['Q']:
+            self.camera.moveUpDown(velocity)
+        elif self.qweasd['E']:
+            self.camera.moveUpDown(-velocity)
+
     def key_event(self, key, action, modifiers):
         self.imgui.key_event(key, action, modifiers)
+        # print('key_event, {}, {}, {}'.format(key, action, modifiers))
+        if key == self.wnd.keys.W:
+            if action == self.wnd.keys.ACTION_PRESS:
+                self.qweasd['W'] = True
+            else:
+                self.qweasd['W'] = False
+        if key == self.wnd.keys.S:
+            if action == self.wnd.keys.ACTION_PRESS:
+                self.qweasd['S'] = True
+            else:
+                self.qweasd['S'] = False
+        if key == self.wnd.keys.A:
+            if action == self.wnd.keys.ACTION_PRESS:
+                self.qweasd['A'] = True
+            else:
+                self.qweasd['A'] = False
+        if key == self.wnd.keys.D:
+            if action == self.wnd.keys.ACTION_PRESS:
+                self.qweasd['D'] = True
+            else:
+                self.qweasd['D'] = False
+        if key == self.wnd.keys.Q:
+            if action == self.wnd.keys.ACTION_PRESS:
+                self.qweasd['Q'] = True
+            else:
+                self.qweasd['Q'] = False
+        if key == self.wnd.keys.E:
+            if action == self.wnd.keys.ACTION_PRESS:
+                self.qweasd['E'] = True
+            else:
+                self.qweasd['E'] = False
 
     def mouse_position_event(self, x, y, dx, dy):
         self.imgui.mouse_position_event(x, y, dx, dy)
 
+        # print('mouse_position_event, {}, {}, {}, {}'.format(x, y, dx, dy))
+
     def mouse_drag_event(self, x, y, dx, dy):
         self.imgui.mouse_drag_event(x, y, dx, dy)
+        # print('mouse_drag_event, {}, {}, {}, {}'.format(x, y, dx, dy))
+        if self.mouseRightPress:
+            current_x = dx * self.SENSITIVITY
+            current_y = dy * self.SENSITIVITY
+            # ----> +x
+            # |
+            # v  +y
+            # yaw此时绕世界坐标系旋转，而不是local的
+            self.camera.yawGlobal(current_x)
+            self.camera.pitch(current_y)
+
 
     def mouse_scroll_event(self, x_offset, y_offset):
         self.imgui.mouse_scroll_event(x_offset, y_offset)
+        # print('mouse_scroll_event, {}, {}'.format( x_offset, y_offset))
 
     def mouse_press_event(self, x, y, button):
         self.imgui.mouse_press_event(x, y, button)
+        # print('mouse_press_event, {}, {}, {}'.format(x, y, button))
+        if button == 1:
+            self.mouseLeftPress = True
+        elif button == 2:
+            self.mouseRightPress = True
+
 
     def mouse_release_event(self, x: int, y: int, button: int):
         self.imgui.mouse_release_event(x, y, button)
+        # print('mouse_release_event, {}, {}, {}'.format(x, y, button))
+        if button == 1:
+            self.mouseLeftPress = False
+        elif button == 2:
+            self.mouseRightPress = False
 
     def unicode_char_entered(self, char):
         self.imgui.unicode_char_entered(char)
