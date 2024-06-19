e255f7f09087c33e538e1131f0790f092bb5fc2d
sheng <shengwangandy@foxmail.com>
2024-06-19 15:24:01 +0800

    补充入参

--- Pygame-OpenGL-3D-Engine/main.py
+++ Pygame-OpenGL-3D-Engine/main.py
@@ -5,6 +5,7 @@ from source.model import CustomMesh
 import os
 import argparse
 import platform
+import sys
 from PIL import Image
 class CustomApp(Engine):
 
@@ -67,6 +68,26 @@ class CustomApp(Engine):
 
 if __name__ == "__main__":
     ## 对于有需要外部指定部分参数的情况，可以把 run_window_config 放到外面来，控制输入到 CustomApp的比那辆
+    ## 入参控制也可以参考：https://github.com/moderngl/moderngl-window/blob/master/examples/modify_parser.py
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-width",
+        "--width",
+        type=int,
+        default=1920,
+        help="render width",
+        required=False,
+    )
+    parser.add_argument(
+        "-height",
+        "--height",
+        type=int,
+        default=1080,
+        help="render height",
+        required=False,
+    )
+    values = parser.parse_args(sys.argv[1:])
 
     ## 不走offscreen
     mglw.run_window_config(config_cls=CustomApp )
